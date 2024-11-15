3971f24a6efe8e3c2ad8d4d1673b50fcf4e841e0
mesmith75 <mark.smith@cern.ch>
2024-11-15 19:56:51 +0000

    updates

--- moments.py
+++ moments.py
@@ -8,6 +8,14 @@ def calculateAllMoments(inframe):
     """
     
     inframe['moment_FL'] = 2. - 2.5*(1. - inframe['ctk']**2)
+    inframe['moment_M1s'] = (1. - inframe['ctk']**2)
+    inframe['moment_M1c'] = inframe['ctk']**2
+    inframe['moment_M2s'] = (1. - inframe['ctk']**2) * (2.*inframe['ctl']**2 - 1.)
+    inframe['moment_M2c'] = inframe['ctk']**2 * (2.*inframe['ctl']**2 - 1.)
+    inframe['moment_S1s'] = 0.0625*(21.*inframe['moment_M1s'] - 14.*inframe['moment_M1c'] + 15.*inframe['moment_M2s'] - 10.*inframe['moment_M2c'])
+    inframe['moment_S1c'] = 0.125*(-7.*inframe['moment_M1s'] + 28.*inframe['moment_M1c'] - 5.*inframe['moment_M2s'] + 20.*inframe['moment_M2c'])
+    inframe['moment_S2s'] = 0.0625*(15.*inframe['moment_M1s'] - 10.*inframe['moment_M1c'] + 45.*inframe['moment_M2s'] - 30.*inframe['moment_M2c'])
+    inframe['moment_S2c'] = 0.125*(-5.*inframe['moment_M1s'] + 20.*inframe['moment_M1c'] - 15.*inframe['moment_M2s'] + 60.*inframe['moment_M2c'])
     inframe['moment_S3'] = 3.125*(1. - inframe['ctk']**2)*(1. - inframe['ctl']**2)*np.cos(2.*inframe['phi'])
     inframe['moment_S5'] = 2.5 * np.sqrt(1. - inframe['ctl']*inframe['ctl']) * 2 * inframe['ctk'] * np.sqrt(1. - inframe['ctk']*inframe['ctk']) * np.cos(inframe['phi'])
     inframe['moment_S4'] = 3.125*2*inframe['ctl'] * np.sqrt(1. - inframe['ctl']*inframe['ctl']) * 2 * inframe['ctk'] * np.sqrt(1. - inframe['ctk']*inframe['ctk']) * np.cos(inframe['phi'])
@@ -21,6 +29,21 @@ def calculateAllVariances(inframe, indices, vals, n_neighbours):
     _i = 0
     for _ind in indices:
         rs = inframe.iloc[_ind]
+        
+        angles_m1s = 1./(n_neighbours*(n_neighbours-1)) * np.sum((vals['M1s'] - (1. - rs['ctk']**2))**2)
+        angles_m1c = 1./(n_neighbours*(n_neighbours-1)) * np.sum((vals['M1c'] - (rs['ctk']**2))**2)
+        angles_m2s = 1./(n_neighbours*(n_neighbours-1)) * np.sum((vals['M2s'] - (1. - rs['ctk']**2) * (2*inframe['ctl']**2 - 1.))**2)
+        angles_m2c = 1./(n_neighbours*(n_neighbours-1)) * np.sum((vals['M2c'] - (rs['ctk']**2) * (2*inframe['ctl']**2 - 1.))**2)
+
+        vals.append["err_M1s"].append(np.sqrt(angles_m1s))
+        vals.append["err_M1c"].append(np.sqrt(angles_m1c))
+        vals.append["err_M2s"].append(np.sqrt(angles_m2s))
+        vals.append["err_M2c"].append(np.sqrt(angles_m2c))
+        
+        vals["err_S1s"] = np.sqrt(0.0625**2 * (441. * angles_m1s + 196. * angles_m1c + 225.*angles_m2s + 100.*angles_m2c))*vals["S1s"][_i]
+        vals["err_S1c"] = np.sqrt(0.125**2 * (49. * angles_m1s + 784. * angles_m1c + 25.*angles_m2s + 400.*angles_m2c))*vals["S1c"][_i]
+        vals["err_S2s"] = np.sqrt(0.0625**2 * (225. * angles_m1s + 100. * angles_m1c + 2025.*angles_m2s + 900.*angles_m2c))*vals["S2s"][_i]
+        vals["err_S2c"] = np.sqrt(0.125**2 * (25. * angles_m1s + 400. * angles_m1c + 225.*angles_m2s + 3600.*angles_m2c))*vals["S2c"][_i]
     
         angles2 = 0.8 - 0.4*vals["FL"][_i] - (1. - rs['ctk']**2)
         vals["err_FL"].append(2.5*np.sqrt( 1./(n_neighbours*(n_neighbours-1)) * np.sum(angles2**2)))
@@ -55,8 +78,8 @@ def calculateOptimisedObservables(inframe):
     This is a function that calculates the optimised observables from the regular ones.
     Does this as a DataFrame for maximum speed
     """
-    inframe["S2s"] = 0.25*(1. - inframe["moment_FL"])
-    inframe["S2c"] = -1. * inframe["moment_FL"]
+    #inframe["S2s"] = 0.25*(1. - inframe["moment_FL"])
+    #inframe["S2c"] = -1. * inframe["moment_FL"]
     inframe["denom"] = np.sqrt(-1.*inframe["S2s"]*inframe["S2c"])
     
     inframe["P1"] = 0.5*(inframe["moment_S3"]/inframe["S2s"])
@@ -69,7 +92,7 @@ def calculateOptimisedObservables(inframe):
     
     return inframe
 
-def calculateOptimisedObservable(siarr, flarr, obs):
+def calculateOptimisedObservable(siarr, flarr, s2sarr, s2carr, obs):
     
     optimisedObsTranslator = {'FL' : 'FL', 'S3' : 'P1', 'S4' : 'P4p', 'S5' : 'P5p', 'AFB' : 'P2', 'S7' : 'P6p', 'S8' : 'P8p', 'S9' : 'P3'}
     
@@ -78,11 +101,15 @@ def calculateOptimisedObservable(siarr, flarr, obs):
         tobs = optimisedObsTranslator[obs]
     flarr = np.array(flarr)
     siarr = np.array(siarr)
-    s2sarr = 0.25*(1. - flarr)
-    s2carr = -1.*flarr
-    denomarr = np.sqrt(flarr*s2sarr)
+    s2sarr = np.array(s2sarr)#0.25*(1. - flarr)
+    s2carr = np.array(s2carr)#-1.*flarr
+    denomarr = np.sqrt(-s2carr*s2sarr)
     if tobs == "FL":
         return flarr
+    elif tobs == "S2c":
+        return s2carr
+    elif tobs == "S2s":
+        return s2sarr
     elif tobs == 'P1':
         retarr = 0.5*(siarr/s2sarr)
     elif tobs == 'P2':
